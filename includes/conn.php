10acaa0f20084b5bfa1dad2c194a6b2636ec4f38
unknown <rmoymoy21@gmail.com>
2022-06-30 22:28:28 +0800

    database connection cofigure

--- includes/conn.php
+++ includes/conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'votesystem');
+	$conn = new mysqli('n4m3x5ti89xl6czh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'aee56nofb16uxtl5', 'ngb1d10q1qsa7isd', 'u3m46cqjx645r6k3');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
